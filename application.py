116b19fbe1dbb9f830348888d1a794960946bc6c
nahid <nahidtop@gmail.com>
2020-02-11 19:58:08 +0100

    enabled caching

--- application.py
+++ application.py
@@ -16,12 +16,12 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Ensure responses aren't cached
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
+# @app.after_request
+# def after_request(response):
+#     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     response.headers["Expires"] = 0
+#     response.headers["Pragma"] = "no-cache"
+#     return response
 
 # Check for environment variable
 if not os.environ.get("DATABASE_URL"):
